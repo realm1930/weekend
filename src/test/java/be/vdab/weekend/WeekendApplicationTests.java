97fca2bc9075338d68e1f374684969950e233695
Arne <a.vaneycken@gmail.com>
2021-02-16 11:04:58 +0100

    Initial commit

---
+++ src/test/java/be/vdab/weekend/WeekendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.weekend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
